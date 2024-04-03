58db7be4696ea1e9db264997fe60656fdeb4682b
scott <scottsnyder@vivaldi.net>
2024-04-03 10:50:26 -0700

    waybar config

--- home/linux/gui/waybar/waybar.nix
+++ home/linux/gui/waybar/waybar.nix
@@ -1,6 +1,6 @@
 {config, pkgs,lib,...}:{
   programs.waybar = {
     enable = true;
-    #settings = lib.importJSON ./waybar-config.jsonc;
+    settings = lib.importJSON ./waybar-config.jsonc;
   };
 }
