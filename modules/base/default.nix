c844a182266b8226e26e17dca0ecbe6979ba63b3
scott <scottsnyder@vivaldi.net>
2024-04-03 13:27:46 -0700

    mylib addition

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -1,12 +1,3 @@
 {mylib, ...}: {
   imports = mylib.scanPaths ./.;
-#  imports = [
-#    ./core.nix
-#    ./dump.nix
-#    ./i18n.nix
-#    ./nix.nix
-#    ./packages.nix
-#    ./user-group.nix
-#    ./nvidia.nix
-#  ];
 }
