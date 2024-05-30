1ae4918e60d1d27478bf11dbbfb0f3aedbf82b1d
scott <scottsnyder@vivaldi.net>
2024-05-30 09:08:08 -0700

    hyprland baseline

--- home/home.nix
+++ home/home.nix
@@ -12,4 +12,8 @@
 
   programs.home-manager.enable = true;
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+    extraConfig = builtins.readFile ../modules/nixos/gui/hyprland/hyprland.conf;
+  };
 }
