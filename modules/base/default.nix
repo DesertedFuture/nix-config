f9babfae91a1b1306422d2b0f6f3e34ec9e77c48
Scott S <scottsnyder@vivaldi.net>
2024-06-19 08:34:43 -0700

    ssh agent added

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -1,4 +1,4 @@
 {...}: {
  # imports =  myLib.scanPaths ./.;
- imports = [./core.nix ./i18n.nix ./nix.nix ./packages.nix ./user-group.nix ./core.nix ./dump.nix ./nvidia.nix ];
+ imports = [./core.nix ./i18n.nix ./nix.nix ./packages.nix ./user-group.nix ./core.nix ./dump.nix ./nvidia.nix ./ssh_agent.nix];
 }
---
+++ modules/base/ssh_agent.nix
@@ -0,0 +1,3 @@
+{...}:{
+    progrmas.ssh.startAgent = true;
+}
